85d32c66f9b8d7f340f496e2367991d786780c0f
kinetic-T <kin3tic.t@gmail.com>
2023-12-28 12:06:45 +0100

    Included format specifier

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 
-printf("Size of a char: "sizeof(char)"byte(s)\n");
-printf("Size of a char: "sizeof(int)"byte(s)\n");
-printf("Size of a char: "sizeof(long int)"byte(s)\n");
-printf("Size of a char: "sizeof(long long int)"byte(s)\n");
-printf("Size of a char: "sizeof(float)"byte(s)\n");
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of a int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long intr));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 return (0);
 }
