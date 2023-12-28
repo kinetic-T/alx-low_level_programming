ebfa84d2f9088a0020f8e368d9a1aad2210ebd07
kinetic-T <kin3tic.t@gmail.com>
2023-12-28 11:44:31 +0100

    Print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: program uses printf() to print strings
+ * Return: always returns 0
+ */
+
+int main(void)
+{
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
