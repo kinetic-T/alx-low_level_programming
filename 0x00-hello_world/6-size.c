2715d49093271f7dc76fa12541593369dfde2a66
kinetic-T <kin3tic.t@gmail.com>
2023-12-28 11:55:15 +0100

    Print sizes of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints size of various types
+ * Return: always returns 0
+ */
+
+int main(void)
+{
+
+printf("Size of a char: "sizeof(char)"byte(s)\n");
+printf("Size of a char: "sizeof(int)"byte(s)\n");
+printf("Size of a char: "sizeof(long int)"byte(s)\n");
+printf("Size of a char: "sizeof(long long int)"byte(s)\n");
+printf("Size of a char: "sizeof(float)"byte(s)\n");
+
+return (0);
+}
