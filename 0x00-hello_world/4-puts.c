864428876413a0c386e4ac693272e66e81de2f0a
kinetic-T <kin3tic.t@gmail.com>
2023-12-28 11:40:28 +0100

    Using puts to print strings

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: program uses puts() to print strings
+ * Return: always returns 0
+ */
+
+int main(void)
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
