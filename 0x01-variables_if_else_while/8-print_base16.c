81e196209c9c182dcb5ad373b52304d8b76e0352
kinetic-T <kin3tic.t@gmail.com>
2023-12-29 12:07:59 +0100

    Print in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: using putchar to print base 16
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char n;
+	char l;
+
+	n = '0';
+	do {
+		putchar (n);
+		n++;
+	} while (n <= '9');
+	
+	l = 'a';
+	while (l <= 'f')
+	{
+		putchar (l);
+		l++;
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
