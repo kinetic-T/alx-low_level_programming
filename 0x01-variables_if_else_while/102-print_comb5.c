64520f4e555e6b1221c8f66316ebdb3795cd76c9
kinetic-T <kin3tic.t@gmail.com>
2023-12-31 00:28:15 +0100

    Print 4 digit combos

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: print 4 digit combos
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n, first, second, third, fourth;
+
+	n = 0;
+	while (n < 10000)
+	{
+		first = (n / 100) / 10;
+		second = (n / 100) % 10;
+		third = (n % 100) / 10;
+		fourth = n % 10;
+		if (second < fourth && first <= third)
+		{
+			putchar ('0' + first);
+			putchar ('0' + second);
+			putchar (' ');
+			putchar ('0' + third);
+			putchar ('0' + fourth);
+			if (n < 9899)
+			{
+				putchar (',');
+				putchar (' ');
+			}
+		}
+		n++;
+	}
+	putchar ('\n');
+	return (0);
+}
