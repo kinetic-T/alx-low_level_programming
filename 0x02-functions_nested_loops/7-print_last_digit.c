58e8498d0a12dcfc88d9b2c4f5218bb1ac9ae736
kinetic-T <kin3tic.t@gmail.com>
2023-12-31 22:41:35 +0100

    Print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function name
+ * @n: number to check
+ * Description: print last digit
+ * Return: returns last digit
+ */
+
+int print_last_digit(int n)
+{
+	int value;
+
+	if (n > 0)
+	{
+		value = n % 10;
+		_putchar('0' + value);
+		return (value);
+	}
+	else if  n == 0)
+	{
+		value = n;
+		_putchar('0' + value);
+		return (value);
+	}
+	else
+	{
+		value = (-1 * n) % 10;
+		_putchar('0' + value);
+		return (value);
+	}
+}
