14499f5b056534922e610104bdb6b55077919ef1
kinetic-T <kin3tic.t@gmail.com>
2024-01-01 00:46:55 +0100

    Corrected the condtn for n

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		while (n != 99)
+		while (n >= 98)
 		{
 			printf("%d", n);
 			if (n != 98)
@@ -25,7 +25,7 @@ void print_to_98(int n)
 		printf("%d\n", n);
 	else if (n < 98)
 	{
-		while (n != 99)
+		while (n <= 98)
 		{
 			printf("%d", n);
 			if (n != 98)
