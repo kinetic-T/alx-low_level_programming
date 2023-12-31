57d39fb1f35a8c58f5964d6774c5cc6a79aaeaff
kinetic-T <kin3tic.t@gmail.com>
2023-12-31 22:25:17 +0100

    Print abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - function name
+ * @n: number to check
+ * Description: prints absolute value
+ * Return: returns int
+ */
+
+int _abs(int n)
+{
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-1 * n);
+	}
+}
