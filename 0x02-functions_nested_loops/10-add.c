749e59ef79942e4d166c338191a32562c8a57b58
kinetic-T <kin3tic.t@gmail.com>
2024-01-01 00:19:08 +0100

    sum 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - function name
+ * @num1: first number
+ * @num2: second number
+ * Description: adds 2 int
+ * Return: return soln
+ */
+
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+}
