4aeebd4ab5f711933fd5d1b0063d8d4c0a3c6e35
kinetic-T <kin3tic.t@gmail.com>
2024-01-02 23:43:37 +0100

    Print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - func to print string
+ * @str: pointer to str
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *str)
+{
+	int i, len;
+
+	i = 0;
+	len = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+		len++;
+	}
+	while (str[len] >= 0)
+	{
+		_putchar(str[len]);
+		len--;
+	}
+	_putchar('\n');
+}
