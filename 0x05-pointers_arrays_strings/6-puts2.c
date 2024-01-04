ad0e2dd73584e07f769362dd86637db92a04e011
kinetic-T <kin3tic.t@gmail.com>
2024-01-04 19:54:18 +0100

    Fixed the null terminator issue

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,10 +12,14 @@ void puts2(char *str)
 	int i;
 
 	i = 0;
-	while (str[i] != '\0')
-	{
+	do {
 		_putchar(str[i]);
 		i += 2;
-	}
+		if (str[i + 1] == '\0')
+		{
+			_putchar(str[i]);
+			break;
+		}
+	} while (str[i] != '\0');
 	_putchar('\n');
 }
