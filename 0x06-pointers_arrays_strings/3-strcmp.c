6a77f78999593d8a392bc50afbbd689ad9a38f84
kinetic-T <kin3tic.t@gmail.com>
2024-01-05 10:25:31 +0100

    used the right ascii arithmetic

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,18 +16,18 @@ int _strcmp(char *s1, char *s2)
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] > s2[i])
-			return (15);
+			return (s1[i] - s2[i]);
 		else if (s1[i] < s2[i])
-			return (-15);
+			return (s1[i] - s2[i]);
 		i++;
 	}
 
 	/* if strings have varied lengths */
-	if (s1[i] == '\0' && s2[i] != '\0')
+/*	if (s1[i] == '\0' && s2[i] != '\0')*/
 		/* s1 is shorter */
-		return (-15);
-	else if (s1[i] != '\0' && s2[i] == '\0')
+/*		return (-15);*/
+/*	else if (s1[i] != '\0' && s2[i] == '\0')*/
 		/* s1 is longer */
-		return (1);
-	return (0);
+/*		return (1);*/
+	return (s1[i] - s2[i]);
 }
