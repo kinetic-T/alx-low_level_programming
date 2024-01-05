1d6a25a9837b54424abd1e851997a09dcc149d45
kinetic-T <kin3tic.t@gmail.com>
2024-01-05 09:42:28 +0100

    Compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * _strcmp - compares strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: return 1 if +ve, -1 if -ve, 0 if equal
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] > s2[i])
+			return (1);
+		else if (s1[i] < s2[i])
+			return (-1);
+		i++;
+	}
+	return (0);
+}
